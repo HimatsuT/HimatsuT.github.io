cf424b754139c8129e268894f9dfbeef4df257ee
HimatsuT <nhattung0205@gmail.com>
2019-07-09 10:10:46 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,125 +1,168 @@
-    
 const screen = document.getElementById("screen");
-let result = "";
-function In(obj) {
-    var str = screen.value;
-    var array = str.split(' ');
-    var lastVal = array[array.length - 1];
-    var regex = /^([+]|[-]|[x]|[÷])$/g;
-    if (str === "0" || str === "Error" || str === "Infinity" || result !== "") {
-        str = "";
-        result = "";
-    }
-    if(obj === '.' && lastVal.includes('.')){
-        return;
-    }
-    if (lastVal.match(regex) !== null) {
-        screen.value += ` ${obj}`;
-    }
-    else {
-        screen.value = Decimal(str, obj);
-    }
-    screen.scrollLeft = screen.scrollWidth;
-}
+        let result = "";
 
-function Operator(obj) {
-    var str = screen.value;
-    var array = str.split(' ');
-    var lastVal = array[array.length - 1];
-    if (obj !== lastVal) {
-        if (lastVal === "+" || lastVal === "-" || lastVal === "x" || lastVal === "÷") {
-            str = str.slice(0, str.length - 2);
+        function In(obj) {
+            var str = screen.value;
+            var array = str.split(' ');
+            var lastVal = array[array.length - 1];
+            var regex = /^([+]|[-]|[x]|[÷])$/g;
+            if (str === "0" || str === "Error" || str === "Infinity" || result !== "") {
+                str = "";
+                result = "";
+            }
+            if (obj === '.' && lastVal.includes('.')) {
+                return;
+            }
+            if (lastVal.match(regex) !== null) {
+                screen.value += ` ${obj}`;
+            }
+            else {
+                screen.value = Decimal(str, obj);
+            }
+            screen.scrollLeft = screen.scrollWidth;
         }
-        screen.value = str + ` ${obj}`;
-    }
-    screen.scrollLeft = screen.scrollWidth;
 
-}
-function Decimal(str, obj) {
-    str += obj;
-    var b = str.replace(/[0-9]+/g,",");
-    for (let i = 0; i < b.length; i++) {
-            if (b[i] === b[i + 1]) {
-            str = str.substring(0, str.length -1);
-        }      
-    }
-    return str;
-}
-
-function Equal() {
-    var a = screen.value;
-    var array = a.split(' ');
-    var Result = 0;
-    Result = array.reduce((Number, Operater, Index) => {
-        var Result;
-        var NextValue = array[Index + 1];
-        switch (Operater) {
-            case "x":
-                Result = +Number * +NextValue;
-                return Result;
-                break;
-            case "÷":
-                Result = +Number / +NextValue;
-                return Result;
-                break;
-            case "+":
-                Result = +Number + +NextValue;
-                return Result;
-                break;
-            case "-":
-                Result = +Number - +NextValue;
-                return Result;
-                break;        
-            default:
-                return Number;
-                break;
+        function Operator(obj) {
+            var str = screen.value;
+            var array = str.split(' ');
+            var lastVal = array[array.length - 1];
+            if (obj !== lastVal) {
+                if (lastVal === "+" || lastVal === "-" || lastVal === "x" || lastVal === "÷") {
+                    str = str.slice(0, str.length - 2);
+                }
+                screen.value = str + ` ${obj}`;
+            }
+            screen.scrollLeft = screen.scrollWidth;
         }
-        return Number + Operator;
-    })
-    if (isNaN(Result)) {
-        return screen.value = "Error";
-    }
-    else {
-        screen.value = Math.round(Result * 1000) / 1000;
-        result = 1;
-    }
-}
-function Check(){}
-function Clear() {
-    var a = document.getElementById("screen");
-    a.value = 0;
-}
-
-function ClearOne(){
-    var str = screen.value;
-    var a = str.split('');
-    var lastVal = a[a.length - 1];    
-    console.log(str);
-    if(lastVal === str ){         
-       str = 0;
-       console.log(str)
-    }
-    else {
-        if(lastVal === " "){
-            str = str.slice(0,str.length - 3);
+        function Decimal(str, obj) {
+            str += obj;
+            var b = str.replace(/[0-9]+/g, ",");
+            for (let i = 0; i < b.length; i++) {
+                if (b[i] === b[i + 1]) {
+                    str = str.substring(0, str.length - 1);
+                }
+            }
+            return str;
         }
-        else if(lastVal === "+" || lastVal === "-" || lastVal === "x" || lastVal === "÷"){
-                str = str.slice(0,str.length - 2);
+
+        function Equal() {
+            var a = screen.value;
+            var array = a.split(' ');
+            function bodmasNeeded(arr) {
+                if ((arr.indexOf('x') !== -1 || arr.indexOf('÷') !== -1) &&
+                    (arr.indexOf('+') !== -1 || arr.indexOf('-') !== -1)) {
+                    return true;
+                }
+                return false;
+            }
+            function orderOperations(arr) {
+                const cleanedArr = arr.map(function (value) {return value;}); 
+    
+                for (let i = 0; i < cleanedArr.length; i += 1) 
+                {
+                    let newVal;
+                    let cleanedVal;
+            
+                    if (!bodmasNeeded(cleanedArr)) 
+                    {
+                        return cleanedArr;
+                    }
+                
+                    const val = cleanedArr[i];
+            
+                    if (i % 2 !== 0 && (val === 'x' || val === '÷')) 
+                    {
+                        switch (val) 
+                        {
+                            case 'x':
+                            newVal = +cleanedArr[i - 1] * +cleanedArr[i + 1];
+                            cleanedVal = newVal;
+                            cleanedArr.splice(i - 1, 3, cleanedVal);
+                            break;
+                            case '÷':
+                            newVal = +cleanedArr[i - 1] / +cleanedArr[i + 1];
+                            cleanedVal = newVal;
+                            cleanedArr.splice(i - 1, 3, cleanedVal);
+                            break;
+                            default: screen.value = 'Error';
+                        }
+
+                        i = -1;
+                    }
+                }
+    
+                return cleanedArr;
+            }    
+                if (bodmasNeeded(array)) {
+                array = orderOperations(array);
+                }
+            var Result = 0;
+            Result = array.reduce((Number, Operater, Index) => {
+                var Result;
+                var NextValue = array[Index + 1];
+                let cleanedVal;
+                switch (Operater) {
+                    case "x":
+                        Result = +Number * +NextValue;
+                        cleanedVal = Result;
+                        return cleanedVal;
+                        // return Result;
+                        break;
+                    case "÷":
+                        Result = +Number / +NextValue;
+                        cleanedVal = Result;
+                        return cleanedVal;
+                        // return Result;
+                        break;
+                    case "+":
+                        Result = +Number + +NextValue;
+                        cleanedVal = Result;
+                        return cleanedVal;
+                        // return Result;
+                        break;
+                    case "-":
+                        Result = +Number - +NextValue;
+                        cleanedVal = Result;
+                        return cleanedVal;
+                        // return Result;
+                        break;
+                    default:
+                        return Number;
+                        break;
+                }
+                return Number + Operater;
+            })
+            if (isNaN(Result)) {
+                return screen.value = "Error";
+            }
+            else {
+                screen.value = Math.round(Result * 1000) / 1000;
+                result = 1;
+            }
         }
-        else{
-            str = str.slice(0,str.length - 1);
+        function Clear() {
+            var a = document.getElementById("screen");
+            a.value = 0;
         }
-    }
-    screen.value = str;
-}
 
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 800);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+        function ClearOne() {
+            var str = screen.value;
+            var a = str.split('');
+            var lastVal = a[a.length - 1];
+            if (lastVal === str) {
+                str = 0;
+                console.log(str)
+            }
+            else {
+                if (lastVal === " ") {
+                    str = str.slice(0, str.length - 3);
+                }
+                else if (lastVal === "+" || lastVal === "-" || lastVal === "x" || lastVal === "÷") {
+                    str = str.slice(0, str.length - 2);
+                }
+                else {
+                    str = str.slice(0, str.length - 1);
+                }
+            }
+            screen.value = str;
+        }
